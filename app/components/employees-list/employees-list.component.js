92583e740e68ea79989194d89674ebbbd0cc90da
hussein shalaby <drshalaby22@gmail.com>
2022-06-04 07:07:23 +0200

    feat: add table search with highlighting the results and employees sorting depending on the performance

--- app/components/employees-list/employees-list.component.js
+++ app/components/employees-list/employees-list.component.js
@@ -1,5 +1,15 @@
 angular
   .module('appModule')
+  .controller('EmployeesListComponent', EmployeesListComponent)
+  .filter('highlight', function ($sce, $location) {
+    return function (text, phrase) {
+      if (phrase) {
+        text = text.replace(new RegExp(phrase, 'gi'), '<span class=c-users-list__hightlighted>$&</span>');
+        $location.search('filter', phrase);
+      }
+      return $sce.trustAsHtml(text);
+    };
+  })
   .component('employeesList', {
     templateUrl: 'components/employees-list/employees-list.html',
     controller: EmployeesListComponent,
@@ -9,4 +19,10 @@ angular
     },
   });
 
-function EmployeesListComponent() {}
+function EmployeesListComponent($scope, $rootScope) {
+  $rootScope.$watch('users', function (newValue, oldValue) {
+    if (newValue !== oldValue) {
+      $scope.search = $rootScope.searchText;
+    }
+  });
+}
