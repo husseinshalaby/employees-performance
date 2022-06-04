4e55b9d8ae5ec8166c61dd88432d5aa03419532d
hussein shalaby <drshalaby22@gmail.com>
2022-06-04 07:17:26 +0200

    feat: edit the chart line and the tooltip to match the design

--- app/components/vue-components/performance-chart.vue
+++ app/components/vue-components/performance-chart.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="c-chart__container">
-    <v-chart ref="chart" :option="chartOptions" />
+    <v-chart ref="chart" :option="chartOptions"/>
   </div>
 </template>
 
@@ -13,107 +13,134 @@ import {
   TitleComponent,
   TooltipComponent,
   GridComponent,
-  VisualMapComponent,
+  VisualMapComponent
 } from "echarts/components";
 import VChart from "vue-echarts";
-
 use([
   CanvasRenderer,
   LineChart,
   TitleComponent,
   TooltipComponent,
   GridComponent,
-  VisualMapComponent,
+  VisualMapComponent
 ]);
-
 export default {
   name: "PerformanceChartComponent",
-
   components: {
-    VChart,
+    VChart
   },
-
   data() {
     return {
       chartData: [
         {
           date_ms: 1641772800000,
-          performance: 0.2,
+          performance: 0.2
         },
         {
           date_ms: 1641859200000,
-          performance: 0.33,
+          performance: 0.33
         },
         {
           date_ms: 1641945600000,
-          performance: 0.53,
+          performance: 0.53
         },
         {
           date_ms: 1642032000000,
-          performance: 0.31,
+          performance: 0.31
         },
         {
           date_ms: 1642118400000,
-          performance: 0.65,
+          performance: 0.65
         },
         {
           date_ms: 1642204800000,
-          performance: 0.88,
+          performance: 0.88
         },
         {
           date_ms: 1642291200000,
-          performance: 0.07,
-        },
-      ],
+          performance: 0.07
+        }
+      ]
     };
   },
-
   computed: {
     initOptions() {
       return {
         width: "auto",
-        height: "300px",
+        height: "300px"
       };
     },
-
     chartOptions() {
       return {
         title: {
           text: "Team Performance Index",
-          left: "center",
+          left: "center"
         },
         tooltip: {
-          trigger: 'axis',
+          trigger: "item",
+          formatter: function(params) {
+            return `${params.name}
+            <br/><span style=background:${
+              params.color
+            };display:inline-block;width:10px;height:10px;border-radius:50%;></span> Team Performance Index: ${
+              params.data
+            }%`;
+          },
           transitionDuration: 0,
+          extraCssText: "text-align: center;",
           confine: false,
           hideDelay: 0,
-          padding: 0,
+          padding: [15, 50],
+          backgroundColor: "#000",
+          textStyle: {
+            color: "#fff"
+          }
         },
         grid: {
           left: "30px",
           right: "12px",
           bottom: "2px",
           top: "6px",
-          containLabel: true,
+          containLabel: true
         },
         xAxis: {
           type: "category",
           showGrid: false,
           data: this.xAxisData,
           axisLine: {
-            show: true,
+            show: true
           },
           axisTick: {
-            show: false,
+            show: false
           },
           axisLabel: {
-            fontSize: 11,
-          },
+            fontSize: 11
+          }
         },
         yAxis: {
           axisLabel: { show: true },
           axisTick: { show: true },
-          splitLine: { show: true },
+          splitLine: { show: true }
+        },
+        visualMap: {
+          top: 50,
+          right: 10,
+          pieces: [
+            {
+              gt: 0,
+              lte: 50,
+              color: "red"
+            },
+            {
+              gt: 50,
+              lte: 80,
+              color: "yellow"
+            },
+            {
+              gt: 80,
+              color: "green"
+            }
+          ]
         },
         series: [
           {
@@ -123,26 +150,23 @@ export default {
             symbolSize: 2,
             cursor: "default",
             lineStyle: {
-              width: 2,
-            },
-          },
-        ],
+              width: 2
+            }
+          }
+        ]
       };
     },
-
     xAxisData() {
-      return this.chartData.map((item) => this.formatDate(item.date_ms));
+      return this.chartData.map(item => this.formatDate(item.date_ms));
     },
-
     yAxisData() {
-      return this.chartData.map((item) => +item.performance * 100);
-    },
+      return this.chartData.map(item => +item.performance * 100);
+    }
   },
-
   methods: {
     formatDate(dateInMs) {
       return moment(dateInMs).format("DD MMM YYYY");
-    },
-  },
+    }
+  }
 };
 </script>
